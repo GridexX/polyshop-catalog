337e4eaf7b93d4094e210b4958d2b177588ffe95
GridexX <arsene582@gmail.com>
2023-04-26 18:30:56 +0200

    feat: add queue listener and sender
    Signed-off-by: GridexX <arsene582@gmail.com>

--- src/main/java/fr/dopolytech/polyshop/catalog/CatalogApplication.java
+++ src/main/java/fr/dopolytech/polyshop/catalog/CatalogApplication.java
@@ -2,7 +2,9 @@ package fr.dopolytech.polyshop.catalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CatalogApplication {
 
